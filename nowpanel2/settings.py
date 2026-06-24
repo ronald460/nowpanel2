860ea14d5a2771200beb376745f61666e38a38ca
Ronald <rgimenez877@gmail.com>
2026-06-24 04:48:05 -0400

    update 11

--- nowpanel2/settings.py
+++ nowpanel2/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'nowpanel2.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
