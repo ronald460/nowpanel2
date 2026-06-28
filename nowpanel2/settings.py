cdce07e4c20f9ae5d0ea6b9c3e092a05ba7c4573
Ronald <rgimenez877@gmail.com>
2026-06-28 16:34:00 -0400

    update 41

--- nowpanel2/settings.py
+++ nowpanel2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', '*', 'nowpanel2.onrender.com']
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/login'
 
