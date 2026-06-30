f2ea479887ed15be6f338f868f6b6640975884fd
Ronald <rgimenez877@gmail.com>
2026-06-29 21:53:13 -0400

    update 56

--- nowpanel2/settings.py
+++ nowpanel2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nowpanel2.onrender.com']
+ALLOWED_HOSTS = ['https://nowpanel2.onrender.com']
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/login'
 
